7c89fd14cbe3f6c03df4d2f87214bde1bddcbe9f
Joey <gibbonsjoey95@gmail.com>
2024-01-15 20:30:36 -0600

    added in round results

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const rockBtn = document.querySelector('#rock')
 const paperBtn = document.querySelector('#paper')
 const scissorsBtn = document.querySelector('#scissors')
-const result = document.querySelector('#result')
 const runningPlayerScore = document.querySelector('#runningPlayerScore')
 const runningComputerScore = document.querySelector('#runningComputerScore')
 const final = document.querySelector('#final')
 const finalResults = document.querySelector('#finalResults')
 const playAgainBtn = document.querySelector('#playAgain')
+const description = document.querySelector('#description')  
+const roundResult = document.querySelector('#roundResult')
 
 let playerScore = 0
 let computerScore = 0
@@ -52,14 +53,20 @@ const playRound = (playerSelection, computerSelection) => {
 
 
 const game = (playerChoice) => {
+    let compueterChoice = getComputerChoice()
+
     let outcome = playRound(playerChoice, getComputerChoice())
+
+    description.textContent = `You chose ${playerChoice}. Computer chose ${getComputerChoice()}`
 
     if(outcome === 'w'){
         playerScore++
         runningPlayerScore.textContent = playerScore
+        roundResult.textContent = "You Win!"
     } else {
         computerScore++
         runningComputerScore.textContent = computerScore
+        roundResult.textContent = "You Lose!"
     }
 
     if(playerScore === 5){
@@ -71,6 +78,7 @@ const game = (playerChoice) => {
         final.classList.remove('hidden')
         finalResults.textContent = `You have lost the series with a score of ${playerScore}. Computer had ${computerScore} `
     }
+
 }
 
 const restartGame = () => {
@@ -78,20 +86,22 @@ const restartGame = () => {
     computerScore = 0
     runningPlayerScore.textContent = 0
     runningComputerScore.textContent = 0
+    description = 'Rock, Paper, or Scissors?'
+    roundResult = 'Choose to continue'
 }
 
-
-
-rockBtn.addEventListener('click', () => {
-    result.textContent = game('Rock')
+rockBtn.addEventListener('mousedown', () => {
+    game('Rock')
 })
 
 paperBtn.addEventListener('click', () => {
-    result.textContent = game('Paper')
+    // result.textContent = game('Paper')
+    game('Paper')
 })
 
 scissorsBtn.addEventListener('click', () => {
-    result.textContent = game('Scissors')
+    // result.textContent = game('Scissors')
+    game('Scissors')
 })
 
 playAgainBtn.addEventListener('click', () => {
